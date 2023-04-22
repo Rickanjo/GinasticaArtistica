1b8474fcc9d1bfba46bd14d6b8e9635a9101f67e
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-21 23:40:22 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o nome do(a) ginasta: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite a nota do(a) ginasta: ");
+double notaGinasta = double.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a nota de execução: ");
+double notaExecucao = double.Parse(Console.ReadLine()!);
+
+
+double notaFinal = notaGinasta + notaExecucao;
+ 
+Console.WriteLine("A nota final de {0} é: {1}", nome, notaFinal);
